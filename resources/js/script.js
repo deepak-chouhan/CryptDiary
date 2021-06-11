77d8ce0219ce5cb20f7b77c24d0bae4dfe88b5e4
deepak <deepakceo0@gmail.com>
2021-06-11 21:03:56 +0530

    fixed nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
+
+
     var mobilenavbar = document.getElementById("mob-link");
     var menu_icon = document.getElementById("menu-button");
 
     var mobile = document.getElementById("mobile-nav");
 
-    mobilenavbar.style.opacity === "0";
-    mobilenavbar.style.height === "0px";
-    mobilenavbar.style.pointerEvents === "none";
+    mobilenavbar.style.opacity = "0";
+    mobilenavbar.style.height = "0px";
+    mobilenavbar.style.pointerEvents = "none";
 
     function navfunctioning() {
-        if (mobilenavbar.style.opacity === "0"){
+        if (mobilenavbar.style.opacity === "0") {
             mobilenavbar.style.height = "100px";
             mobilenavbar.style.opacity = "1";
             mobilenavbar.style.pointerEvents = "all";
