f815e2d9332be7fbf388ef69cdfa74368ae84e25
deepak <deepakceo0@gmail.com>
2021-06-11 16:12:31 +0530

    added responsive navbar

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    var mobilenavbar = document.getElementById("mob-link");
+    var menu_icon = document.getElementById("menu-button");
+
+    var mobile = document.getElementById("mobile-nav");
+
+    // mobilenavbar.style.opacity === "0";
+    mobilenavbar.style.height === "0px";
+
+    function navfunctioning() {
+        if (mobilenavbar.style.opacity === "0" || mobilenavbar.style.height === "0px"){
+            mobilenavbar.style.height = "100px"
+            mobilenavbar.style.opacity = "1";
+            mobilenavbar.style.pointerEvents = "all"
+        } else {
+            mobilenavbar.style.height = "0px"
+            mobilenavbar.style.opacity = "0";
+            mobilenavbar.style.pointerEvents = "none"
+        }
+    }
+
+    $(menu_icon).click(function () {
+        navfunctioning();
+    });
+
+});
